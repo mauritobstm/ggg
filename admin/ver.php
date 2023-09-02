64cf336a405ec6cd322dcef84bb12f98bcc668a5
mauritobstm <132168116+mauritobstm@users.noreply.github.com>
2023-09-02 17:22:24 -0300

    Add files via upload

---
+++ admin/ver.php
@@ -0,0 +1,15 @@
+
+<body>
+<?php
+                    require '../../vendor/autoload.php';
+                    $id = $_GET[':id'];?>
+<div class="modal fade">
+    <div class="col-md-12">
+        <fieldset>
+            <legend>informacion de la compra</legend>
+
+        </fieldset>
+    </div>
+</div>
+
+</body>
